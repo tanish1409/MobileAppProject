8db273d0cc939208e9241599247d6b86c38ac0b2
Kunal Pandya <rrkspandya@gmail.com>
2025-10-26 15:00:33 -0400

    add back button

--- NetWork/app/src/main/java/com/example/network/ClubListActivity.kt
+++ NetWork/app/src/main/java/com/example/network/ClubListActivity.kt
@@ -1,4 +1,19 @@
 package com.example.network
 
-class ClubListActivity {
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+
+class ClubListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_club_list)
+
+        val backBtn = findViewById<Button>(R.id.backBtn)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
 }
