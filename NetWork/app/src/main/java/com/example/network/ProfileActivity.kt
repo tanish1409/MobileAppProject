39bcf73dde99e3bdf84e4a0fd54c33ddfe311a82
kunalpanda <rrkspandya@gmail.com>
2025-10-26 15:52:16 -0400

    add back button to Profile.xml

--- NetWork/app/src/main/java/com/example/network/ProfileActivity.kt
+++ NetWork/app/src/main/java/com/example/network/ProfileActivity.kt
@@ -1,4 +1,20 @@
 package com.example.network
 
-class ProfileActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val backBtn = findViewById<Button>(R.id.backBtn)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
 }
